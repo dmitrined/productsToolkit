703a0c76fcdf4484f233037b26d83c2b8122e613
dmitrined <dm.nedioglo@gmail.com>
2025-11-20 20:44:45 +0100

    products

---
+++ my-toolkit/src/App.tsx
@@ -0,0 +1,16 @@
+
+import type { JSX } from 'react'
+import './App.css'
+import { ProductsList } from './features/products/ProductsList'
+
+function App(): JSX.Element{
+
+  return (
+    <>
+     
+       <ProductsList />
+    </>
+  )
+}
+
+export default App
